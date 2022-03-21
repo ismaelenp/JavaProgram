5dfd1d2357f69e550d3a3adb8733d0e5b89b10ab
ismaelenp <102043878+ismaelenp@users.noreply.github.com>
2022-03-21 15:48:23 +0100

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,9 @@
+# JavaProgram
+public class Jenkins_Ismael_Garcia {
+
+	public static void main(String[] args) {
+		String xxx = "Ismael Garcia Bougrine";
+		System.out.println("Hola Jenkins, sóc l'"+xxx+".");
+	}
+
+}
