4ae633770fa9d81588942adf19398569d0c13af1
ismaelenp <102043878+ismaelenp@users.noreply.github.com>
2022-04-04 15:13:57 +0200

    Update and rename Jenkins_Ismael_Garcia.java to Examen_Ismael_Garcia.java

---
+++ Examen_Ismael_Garcia.java
@@ -0,0 +1,9 @@
+
+public class Examen_Ismael_Garcia.java {
+
+	public static void main(String[] args) {
+		String xxx = "Ismael Garcia Bougrine";
+		System.out.println("Soc"+ xxx +"i estic fent l'examen de la UF2 M01 DAM");
+	}
+
+}
--- Jenkins_Ismael_Garcia.java
+++
@@ -1,9 +0,0 @@
-
-public class Jenkins_Ismael_Garcia {
-
-	public static void main(String[] args) {
-		String xxx = "Ismael Garcia Bougrine";
-		System.out.println("Hola Jenkins, sóc l'"+xxx+".");
-	}
-
-}
