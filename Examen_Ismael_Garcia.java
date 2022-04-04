20260feacbc81c637b0d91eaeedde8a78ad79663
ismaelenp <102043878+ismaelenp@users.noreply.github.com>
2022-04-04 15:28:10 +0200

    Update Examen_Ismael_Garcia.java

--- Examen_Ismael_Garcia.java
+++ Examen_Ismael_Garcia.java
@@ -1,6 +1,4 @@
-
 public class Examen_Ismael_Garcia.java {
-
 	public static void main(String[] args) {
 		System.out.println("Soc Ismael Garcia Bougrine i estic fent l'examen de la UF2 M01 DAM");
 	}
