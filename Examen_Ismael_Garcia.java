10ee8e954b1008352e11041db0fc5bb8999b5ab6
ismaelenp <102043878+ismaelenp@users.noreply.github.com>
2022-04-04 15:25:07 +0200

    Update Examen_Ismael_Garcia.java

--- Examen_Ismael_Garcia.java
+++ Examen_Ismael_Garcia.java
@@ -2,8 +2,7 @@
 public class Examen_Ismael_Garcia.java {
 
 	public static void main(String[] args) {
-		String xxx = "Ismael Garcia Bougrine";
-		System.out.println("Soc"+ xxx +"i estic fent l'examen de la UF2 M01 DAM");
+		System.out.println("Soc Ismael Garcia Bougrine i estic fent l'examen de la UF2 M01 DAM");
 	}
 
 }
